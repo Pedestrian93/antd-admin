663b8f8199a4e135ef7053fd805ee884ca6c613f
Alex San <sanpc@qq.com>
2019-06-27 19:33:57 +0800

    chore: first commit

--- src/pages/antdComponents/table/index.js
+++ src/pages/antdComponents/table/index.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
 
 export default class index extends Component {
+  fetch = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => {
+        console.log(response)
+        response.json()
+      })
+      .then(json => console.log(json))
+  }
+
   render() {
+    this.fetch()
     return <div>table</div>
   }
 }
